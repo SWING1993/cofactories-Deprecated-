4bf77222cbce03b1104515edcae3b5be03cf3ccc
宋国华 <10446627@qq.com>
2015-07-30 10:31:39 +0800

    Header.h修改

--- cofactory-1.1/Header.h
+++ cofactory-1.1/Header.h
@@ -1,27 +1,13 @@
-//
-//  Header.h
-//  cofactory-1.1
-//
-//  Created by Mr.song on 15/7/9.
-//  Copyright (c) 2015年 聚工科技. All rights reserved.
-//
-
 //ios系统版本
 #define ios8x [[[UIDevice currentDevice] systemVersion] floatValue] >=8.0f
 #define ios7x ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0f) && ([[[UIDevice currentDevice] systemVersion] floatValue] < 8.0f)
 #define ios6x [[[UIDevice currentDevice] systemVersion] floatValue] < 7.0f
 #define iosNot6x [[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0f
-
 #define kStatusBarHeight 20
-
 #define kNavigationBarHeight 44
-
 #define iphone4x_3_5 ([UIScreen mainScreen].bounds.size.height==480.0f)
-
 #define iphone5x_4_0 ([UIScreen mainScreen].bounds.size.height==568.0f)
-
 #define iphone6_4_7 ([UIScreen mainScreen].bounds.size.height==667.0f)
-
 #define iphone6Plus_5_5 ([UIScreen mainScreen].bounds.size.height==736.0f || [UIScreen mainScreen].bounds.size.height==414.0f)
 
 //屏幕宽高
@@ -40,24 +26,22 @@
 
 
 //第三方
-#import "HttpClient.h"
-#import "Tools.h"
 #import "MobClick.h"
 #import "UMessage.h"
 #import "MBProgressHUD.h"
 #import "UIButton+WebCache.h"
 #import "UIImageView+WebCache.h"
-#import "JSDropDownMenu.h"
+#import "JSDropDownMenu.h"//筛选
 #import "ODRefreshControl.h"//下拉刷新
 
+//工具类
+#import "HttpClient.h"
+#import "Tools.h"
 
 
 
-//#import "UMFeedback.h"
 #import "AppDelegate.h"
 #import "ViewController.h"
-
-
 
 //登录注册VC
 #import "LoginViewController.h"//登录
@@ -65,24 +49,23 @@
 #import "ResetPasswordViewController.h" //找回密码
 #import "TouristViewController.h" //游客身份
 
-
-
 //HomeVC
 #import "HomeViewController.h"
 #import "HomeEditViewController.h"//主页编辑
 
-
-#import "PushViewController.h"//推送助手
+//推送助手
+#import "PushViewController.h"
 
 //找合作商
 #import "FactoryListViewController.h"
 
-//订单
+//发布订单
 #import "PushOrderViewController.h"//推送订单
-#import "StatusViewController.h"//设置状态
 #import "AddOrderViewController.h"//发布订单
 #import "searchOrderListVC.h"//搜索订单
-
+//设置状态
+#import "StatusViewController.h"
+//订单详情
 #import "OrderListViewController.h"
 #import "OrderListTableViewCell.h"
 #import "OrderDetailViewController.h"
@@ -95,19 +78,17 @@
 #import "VeifyEndViewController.h"//认证成功
 
 
-#import "CooperationViewController.h"
-#import "CooperationInfoViewController.h"//合作商详情页
-
-
 #import "ActivityViewController.h"//营销活动
 #import "WebViewController.h"//抽奖
 
+//合作商详情页
+#import "CooperationViewController.h"
+#import "CooperationInfoViewController.h"
 
 //消息
 #import "MessageViewController.h"
 
-
-//MeVC
+//我
 #import "MeViewController.h"
 #import "SetViewController.h" //设置VC
 #import "RevisePasswordViewController.h"//修改密码
@@ -115,10 +96,10 @@
 #import "ModifyNameViewController.h"//设置name
 #import "ModifyJobViewController.h"//设置job
 #import "SetaddressViewController.h"//工厂位置
-#import "ModifyFactoryNameViewController.h"//设置工厂名称
-#import "SetMapViewController.h"//map
-#import "ModifySizeViewController.h"//修改公司size
-#import "ModifyServiceRangeViewController.h"//修改公司业务类型
+#import "ModifyFactoryNameViewController.h"//工厂名称
+#import "SetMapViewController.h"//地图
+#import "ModifySizeViewController.h"//公司规模
+#import "ModifyServiceRangeViewController.h"//公司业务类型
 #import "PhotoViewController.h"//公司相册
 #import "UploadImageViewController.h"//上传图片
 #import "FavoriteViewController.h"//我的收藏
